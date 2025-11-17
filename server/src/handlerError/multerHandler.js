781fa491058c227865227f541829490f4c6b23e2
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-11-17 13:33:46 +0200

    Created multerHandler

---
+++ server/src/handlerError/multerHandler.js
@@ -0,0 +1,9 @@
+const { MulterError } = require('multer');
+const BadRequestError = require('../errors/BadRequestError');
+
+module.exports = (err, req, res, next) => {
+  if (err instanceof MulterError) {
+    return next(new BadRequestError('Invalid file'));
+  }
+  next();
+};
