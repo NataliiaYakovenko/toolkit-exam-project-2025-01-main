c992846236d0f2e0f5c5ba7441cceef79d0d045f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-11-18 14:09:07 +0200

    Replaced defaultProps with default parameters in Logo component

--- client/src/components/Logo/index.jsx
+++ client/src/components/Logo/index.jsx
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import CONSTANTS from '../../constants.js';
 
-const Logo = ({ to, ...props }) => (
+const Logo = ({
+  to = '/',
+  src = `${CONSTANTS.STATIC_IMAGES_PATH}blue-logo.png`,
+  alt = 'logo',
+  ...props
+}) => (
   <Link to={to}>
-    <img {...props} />
+    <img src={src} alt={alt} {...props} />
   </Link>
 );
 
@@ -14,12 +19,6 @@ Logo.propTypes = {
   to: PropTypes.string,
   src: PropTypes.string,
   alt: PropTypes.string,
-};
-
-Logo.defaultProps = {
-  to: '/',
-  src: `${CONSTANTS.STATIC_IMAGES_PATH}blue-logo.png`,
-  alt: 'logo',
 };
 
 export default Logo;
