23f6769b018159cee07722dc801583676699515e
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-11-28 16:07:18 +0200

    Added correct to burger menu

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -186,153 +186,312 @@ class Header extends React.Component {
             <span></span>
           </div>
 
-          <div className={`${styles.leftNav} ${this.state.isMobileMenuOpen ? styles.mobileMenuOpen : ''}`}>
-            <div className={styles.nav}>
-              <ul>
-                <li>
-                  <span>NAME IDEAS</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>BEAUTY</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>CONSULTING</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>E-COMMERCE</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>FASHION & CLOTHING</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>FINANCE</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>REAL ESTATE</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>TECH</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>MORE CATEGORIES</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <span>CONTESTS</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>HOW IT WORKS</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>PRICING</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>AGENCY SERVICE</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>ACTIVE CONTESTS</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>WINNERS</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>LEADERBOARD</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>BECOME A CREATIVE</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <span>OUR WORK</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>NAMES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>TAGLINES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>LOGOS</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>TESTIMONIALS</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <span>NAMES FOR SALE</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>POPULAR NAMES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>SHORT NAMES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>INTRIGUING NAMES</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>NAMES BY CATEGORY</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>VISUAL NAME SEARCH</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>SELL YOUR DOMAINS</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <span>BLOG</span>
-                  <img
-                    src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
-                    alt="menu"
-                  />
-                  <ul>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>ULTIMATE NAMING GUIDE</a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>
-                        POETIC DEVICES IN BUSINESS NAMING
-                      </a>
-                    </li>
-                    <li>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>CROWDED BAR THEORY</a>
-                    </li>
-                    <li className={styles.last}>
-                      <a href="http://www.google.com" onClick={this.closeMobileMenu}>ALL ARTICLES</a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-            {this.props.data && this.props.data.role !== CONSTANTS.CREATOR && (
-              <div
-                className={styles.startContestBtn}
-                onClick={() => { this.startContests(); this.closeMobileMenu(); }}
-              >
-                START CONTEST
+          <div
+            className={`${styles.mobileMenuOverlay} ${
+              this.state.isMobileMenuOpen ? styles.mobileMenuOpen : ''
+            }`}
+            onClick={this.closeMobileMenu}
+          >
+            <div
+              className={`${styles.leftNav} ${
+                this.state.isMobileMenuOpen ? styles.mobileMenuOpen : ''
+              }`}
+              onClick={(e) => e.stopPropagation()}
+            >
+              <div className={styles.nav}>
+                <ul>
+                  <li>
+                    <span>NAME IDEAS</span>
+                    <img
+                      src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
+                      alt="menu"
+                    />
+                    <ul>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          BEAUTY
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          CONSULTING
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          E-COMMERCE
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          FASHION & CLOTHING
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          FINANCE
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          REAL ESTATE
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          TECH
+                        </a>
+                      </li>
+                      <li className={styles.last}>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          MORE CATEGORIES
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <span>CONTESTS</span>
+                    <img
+                      src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
+                      alt="menu"
+                    />
+                    <ul>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          HOW IT WORKS
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          PRICING
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          AGENCY SERVICE
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          ACTIVE CONTESTS
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          WINNERS
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          LEADERBOARD
+                        </a>
+                      </li>
+                      <li className={styles.last}>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          BECOME A CREATIVE
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <span>OUR WORK</span>
+                    <img
+                      src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
+                      alt="menu"
+                    />
+                    <ul>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          NAMES
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          TAGLINES
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          LOGOS
+                        </a>
+                      </li>
+                      <li className={styles.last}>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          TESTIMONIALS
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <span>NAMES FOR SALE</span>
+                    <img
+                      src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
+                      alt="menu"
+                    />
+                    <ul>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          POPULAR NAMES
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          SHORT NAMES
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          INTRIGUING NAMES
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          NAMES BY CATEGORY
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          VISUAL NAME SEARCH
+                        </a>
+                      </li>
+                      <li className={styles.last}>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          SELL YOUR DOMAINS
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <span>BLOG</span>
+                    <img
+                      src={`${CONSTANTS.STATIC_IMAGES_PATH}menu-down.png`}
+                      alt="menu"
+                    />
+                    <ul>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          ULTIMATE NAMING GUIDE
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          POETIC DEVICES IN BUSINESS NAMING
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          CROWDED BAR THEORY
+                        </a>
+                      </li>
+                      <li className={styles.last}>
+                        <a
+                          href="http://www.google.com"
+                          onClick={this.closeMobileMenu}
+                        >
+                          ALL ARTICLES
+                        </a>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
               </div>
-            )}
+              {this.props.data &&
+                this.props.data.role !== CONSTANTS.CREATOR && (
+                  <div
+                    className={styles.startContestBtn}
+                    onClick={() => {
+                      this.startContests();
+                      this.closeMobileMenu();
+                    }}
+                  >
+                    START CONTEST
+                  </div>
+                )}
+            </div>
           </div>
         </div>
       </div>
