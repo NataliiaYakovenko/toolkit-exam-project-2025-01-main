01268b948b235045e2aa8d8550810c95dc67c1aa
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-12-02 12:16:09 +0200

    Moved constants to module.exports

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
 const serverIP = 'localhost';
 const serverPort = 5000;
-export default {
+module.exports = {
   CUSTOMER: 'customer',
   CREATOR: 'creator',
   CONTEST_STATUS_ACTIVE: 'active',
