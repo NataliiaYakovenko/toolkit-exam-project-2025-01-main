d4f566fb03855fcc92e91ea74d7acdbacfbbf3d3
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-12-06 15:07:08 +0200

    Сreated the structure and styles for HowItWorksHeaderTop

--- client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderTop/HowItWorksHeaderTop.jsx
+++ client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderTop/HowItWorksHeaderTop.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styles from './HowItWorksHeaderTop.module.sass';
 
 class HowItWorksHeaderTop extends React.Component {
   constructor(props) {
@@ -9,40 +9,55 @@ class HowItWorksHeaderTop extends React.Component {
 
   render() {
     return (
-      <>
-        {/* top row */}
-        <div>
-          <div>
-            <h2>Black Friday Sale!</h2>
-            <p>25% Off The Best Domains on The Web</p>
-          </div>
+      <div className={styles.containerTop}>
+        <div className={styles.wrapperBunner}>
+          <div className={styles.bunner}>
+            <div className={styles.leftBunner}></div>
 
-          <div>
-            <p>Hurry! Sale Ends In:</p>
-          </div>
-
-          <div>
-            <div>
-              <p>4</p>
-              <p>DAYS</p>
+            <div className={styles.middleBunner}>
+              <h2 className={styles.title}>Black Friday Sale!</h2>
+              <p className={styles.subtitle}>
+                25% Off The Best Domains on The Web
+              </p>
+              <p className={styles.mobileSubtitle}>Get 25% Off Top Domains.</p>
+              <a
+                className={styles.mobileButtonShopNow}
+                href="https://www.atom.com/black-friday-sale"
+              >
+                SHOP NOW
+              </a>
             </div>
 
-            <div>
-              <p>17</p>
-              <p>HOURS</p>
-            </div>
+            <div className={styles.rightBunner}>
+              <p className={styles.hurryText}>Hurry! Sale Ends In:</p>
+              <p className={styles.mobileHurryText}>Hurry! Ends In:</p>
 
-            <div>
-              <p>58</p>
-              <p>MOUNTS</p>
-            </div>
-          </div>
+              <div className={styles.bannerTimer}>
+                <div className={styles.timerItem}>
+                  <p className={styles.timerNumber}>4</p>
+                  <p className={styles.timerLabel}>DAYS</p>
+                </div>
 
-          <div>
-            <Link to="/shopNow">Shop Now</Link>
+                <div className={styles.timerItem}>
+                  <p className={styles.timerNumber}>17</p>
+                  <p className={styles.timerLabel}>HOURS</p>{' '}
+                </div>
+
+                <div className={styles.timerItem}>
+                  <p className={styles.timerNumber}>58</p>
+                  <p className={styles.timerLabel}>MOUNTS</p>
+                </div>
+              </div>
+              <a
+                className={styles.buttonShopNow}
+                href="https://www.atom.com/black-friday-sale"
+              >
+                Shop Now
+              </a>
+            </div>
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }
