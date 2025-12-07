48c6245ac135a1d4a8b4d2e0173a28af77efdef4
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-12-07 13:11:54 +0200

    Added logo from local public to HowItWorksHeaderLogo

--- client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderButtom/HeaderLogo/HeaderLogo.jsx
+++ client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderButtom/HeaderLogo/HeaderLogo.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
-import styles from './HeaderLogo.module.sass'
+import styles from './HeaderLogo.module.sass';
+import CONSTANTS from '../../../../../constants';
 
 class HowItWorksHeaderLogo extends React.Component {
-
-
   render() {
     return (
       <>
         <div className={styles.headerLogo}>
           <a href="/">
-            <img
-              src="https://img.atom.com/public/images/atom-logo.png"
-              alt="logo"
-            />
+            <img src={CONSTANTS.HOW_IT_WORKS_HEADER_LOGO_ATOM} alt="logo" />
           </a>
         </div>
       </>
