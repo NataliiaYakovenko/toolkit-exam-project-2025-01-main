5a230e2bac3e207ea9e3bf5241c63feda19609fe
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-12-07 23:08:33 +0200

    Added optimization for HowItWorksHeaderButtom

---
+++ client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderButtom/HowItWorksHeaderButtom/HowItWorksHeaderButtom.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import styles from './HowItWorksHeaderButtom.module.sass';
+import HowItWorksHeaderLogo from '../HeaderLogo/HeaderLogo';
+import DomainForSale from '../HeaderNavigation/DomainForSale/DomainForSale';
+import FreeDomainTools from '../HeaderNavigation/FreeDomainTools/FreeDomainTools';
+import NamingBrandingService from '../HeaderNavigation/NamingBrandingService/NamingBrandingService';
+import WyAtom from '../HeaderNavigation/WyAtom/WyAtom';
+
+const HowItWorksHeaderButtom = () => {
+  const [activeTab, setActiveTab] = useState('');
+
+  return (
+    <div className={styles.headerButtom}>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerWrapper}>
+          <div>
+            <HowItWorksHeaderLogo />
+          </div>
+          <div className={styles.navigationContainer}>
+            <div className={styles.navigationWrapper}>
+              <ul>
+                <li>
+                  <div
+                    className={styles.titleDomians}
+                    onMouseEnter={() => setActiveTab('domainForSale')}
+                    onMouseLeave={() => setActiveTab('')}
+                  >
+                    Domains for Sale
+                  </div>
+                  {activeTab === 'domainForSale' && <DomainForSale />}
+                </li>
+
+                <li>
+                  <div
+                    className={styles.titleDomians}
+                    onMouseEnter={() => setActiveTab('freeDomainTools')}
+                    onMouseLeave={() => setActiveTab('')}
+                  >
+                    Free Domain Tools
+                  </div>
+                  {activeTab === 'freeDomainTools' && <FreeDomainTools />}
+                </li>
+
+                <li>
+                  <div
+                    className={styles.titleDomians}
+                    onMouseEnter={() => setActiveTab('namingBrandingService')}
+                    onMouseLeave={() => setActiveTab('')}
+                  >
+                    Naming & Branding Services
+                  </div>
+                  {activeTab === 'namingBrandingService' && (
+                    <NamingBrandingService />
+                  )}
+                </li>
+
+                <li>
+                  <div
+                    className={styles.titleDomians}
+                    onMouseEnter={() => setActiveTab('wyAtom')}
+                    onMouseLeave={() => setActiveTab('')}
+                  >
+                    Wy Atom
+                  </div>
+                  {activeTab === 'wyAtom' && <WyAtom />}
+                </li>
+
+              </ul>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HowItWorksHeaderButtom;
