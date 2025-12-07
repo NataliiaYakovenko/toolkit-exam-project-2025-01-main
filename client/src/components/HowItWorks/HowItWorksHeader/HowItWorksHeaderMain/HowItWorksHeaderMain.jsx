4771964ff4e3afef6cd5054898d8c0aaa79828ed
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-12-07 23:14:06 +0200

    Added optimization for HowItWorksHeaderMain

--- client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderMain/HowItWorksHeaderMain.jsx
+++ client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderMain/HowItWorksHeaderMain.jsx
@@ -1,55 +1,19 @@
 import React from 'react';
 import HowItWorksHeaderTop from '../HowItWorksHeaderTop/HowItWorksHeaderTop';
-import HowItWorksHeaderIcons from '../HowItWorksHeaderIcons/HowItWorksHeaderIcons';
-import HowItWorksHeaderLogo from '../HowItWorksHeaderLogo/HowItWorksHeaderLogo';
-import DomainForSale from '../HowItWorksHeaderNavigation/DomainForSale/DomainForSale';
-import FreeDomainTools from '../HowItWorksHeaderNavigation/FreeDomainTools/FreeDomainTools';
-import NamingBrandingService from '../HowItWorksHeaderNavigation/NamingBrandingService/NamingBrandingService';
-import WyAtom from '../HowItWorksHeaderNavigation/WyAtom/WyAtom';
+import HowItWOrksHeaderButtom from '../HowItWorksHeaderButtom/HowItWorksHeaderButtom/HowItWorksHeaderButtom';
 
 class HowItWorksHeaderMain extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
   render() {
     return (
-      <>
-        {/* top row */}
+      <div>
         <div>
           <HowItWorksHeaderTop />
         </div>
 
-        {/* buttom row */}
         <div>
-          <div>
-            <HowItWorksHeaderLogo />
-          </div>
-
-          {/*  NAV*/}
-          <div>
-            <nav>
-              {/* 1 */}
-              <DomainForSale />
-
-              {/* 2 */}
-              <FreeDomainTools />
-
-              {/* 3 */}
-              <NamingBrandingService />
-
-              {/* 4 */}
-              <WyAtom />
-            </nav>
-          </div>
-
-          {/* icons */}
-          <div>
-            <HowItWorksHeaderIcons />
-          </div>
+          <HowItWOrksHeaderButtom />
         </div>
-      </>
+      </div>
     );
   }
 }
