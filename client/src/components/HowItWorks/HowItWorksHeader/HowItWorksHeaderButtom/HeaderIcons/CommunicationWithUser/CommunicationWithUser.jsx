2c8ac7198a5bea8e782fb5177117da9a9368e63a
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-12-09 13:55:37 +0200

    Added communication form / Added link images to local folder public

---
+++ client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderButtom/HeaderIcons/CommunicationWithUser/CommunicationWithUser.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import UserContactInfo from '../Common/UserContactInfo/UserContactInfo';
+import CONSTANTS from '../../../../../../constants';
+import styles from './CommunicationWithUser';
+
+const CommunicationWithUser = () => {
+  return (
+    <div className={styles.communictionContainer}>
+      <UserContactInfo
+        link="https://www.atom.com/tel:1-877-355-3585"
+        image={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_PHONE}
+        alt="phone"
+        title="(877) 355-3585"
+      />
+      <UserContactInfo
+        link="/"
+        image={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_CHAT}
+        alt="Chat"
+        title="Chat"
+      />
+      <UserContactInfo
+        link="/"
+        image={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_EMAIL}
+        alt="Email"
+        title="Email"
+      />
+      <UserContactInfo
+        link="https://helpdesk.atom.com/en/"
+        image={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_HELP_DESK}
+        alt="Help desk"
+        title="Help Desk"
+      />
+    </div>
+  );
+};
+
+export default CommunicationWithUser;
