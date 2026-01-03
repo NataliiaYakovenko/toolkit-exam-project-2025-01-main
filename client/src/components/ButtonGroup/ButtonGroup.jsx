318882b097a25535000ef8333f276391a5141f8c
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-01-03 13:27:00 +0200

    Added type for button

--- client/src/components/ButtonGroup/ButtonGroup.jsx
+++ client/src/components/ButtonGroup/ButtonGroup.jsx
@@ -15,7 +15,7 @@ const ButtonGroup = () => {
         Do you want a matching domain (.com URL) with your name?
       </h3>
       <div className={styles.answersWrapper}>
-        <button ref={firstButton} className={styles.answerBox}>
+        <button ref={firstButton} className={styles.answerBox} type="button">
           <legend className={styles.answerMark}>Recommended</legend>
           <div className={styles.textWrapper}>
             <img src={CONSTANTS.BUTTON_GROUP_MARK} alt="Mark" />
@@ -23,14 +23,14 @@ const ButtonGroup = () => {
             <p>But minor variations are allowed</p>
           </div>
         </button>
-        <button className={styles.answerBox}>
+        <button className={styles.answerBox} type="button">
           <div className={styles.textWrapper}>
             <img src={CONSTANTS.BUTTON_GROUP_MARK} alt="Mark" />
             <strong>Yes</strong>
             <p>The Domain should exactly match the name</p>
           </div>
         </button>
-        <button className={styles.answerBox}>
+        <button className={styles.answerBox} type="button">
           <div className={styles.textWrapper}>
             <img src={CONSTANTS.BUTTON_GROUP_MARK} alt="Mark" />
             <strong>No</strong>
