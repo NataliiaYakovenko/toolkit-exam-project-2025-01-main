23a76a3a7619d4e61f95a622b7d7e6420bb70e50
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-01-11 13:16:03 +0200

    Changed timestamps in Offer model

--- server/src/models/Offer.js
+++ server/src/models/Offer.js
@@ -1,46 +1,50 @@
-
 module.exports = (sequelize, DataTypes) => {
-  const Offer = sequelize.define('Offers', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER,
+  const Offer = sequelize.define(
+    'Offers',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      contestId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      text: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      fileName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      originalFileName: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      status: {
+        type: DataTypes.ENUM('pending', 'approved', 'rejected'),
+        allowNull: true,
+        defaultValue: 'pending',
+      },
     },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-
+    {
+      timestamps: true,
     },
-    contestId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    text: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    fileName: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    originalFileName: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    status: {
-      type: DataTypes.ENUM('pending', 'approved', 'rejected'),
-      allowNull: true,
-      defaultValue: 'pending',
-    },
-  },
-  {
-    timestamps: false,
-  });
+  );
 
   Offer.associate = function (models) {
     Offer.belongsTo(models.User, { foreignKey: 'userId', sourceKey: 'id' });
-    Offer.belongsTo(models.Contest, { foreignKey: 'contestId', sourceKey: 'id' });
+    Offer.belongsTo(models.Contest, {
+      foreignKey: 'contestId',
+      sourceKey: 'id',
+    });
   };
 
   return Offer;
