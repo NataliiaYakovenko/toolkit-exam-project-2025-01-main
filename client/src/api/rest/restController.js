e1c1e81779d35506e097f9d0d90e0748d8873af6
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-01-18 15:00:47 +0200

    Added requests getOffers and moderateOffer to restController

--- client/src/api/rest/restController.js
+++ client/src/api/rest/restController.js
@@ -1,42 +1,44 @@
 import http from '../interceptor';
 
-export const registerRequest = data => http.post('registration', data);
-export const loginRequest = data => http.post('login', data);
+export const registerRequest = (data) => http.post('registration', data);
+export const loginRequest = (data) => http.post('login', data);
 export const getUser = () => http.get('getUser');
-export const updateContest = data => http.put('updateContest', data);
-export const setNewOffer = data => http.post('setNewOffer', data);
-export const setOfferStatus = data => http.post('setOfferStatus', data);
-export const downloadContestFile = data =>
+export const updateContest = (data) => http.put('updateContest', data);
+export const setNewOffer = (data) => http.post('setNewOffer', data);
+export const setOfferStatus = (data) => http.post('setOfferStatus', data);
+export const downloadContestFile = (data) =>
   http.get(`downloadFile/${data.fileName}`);
-export const payMent = data => http.post('pay', data.formData);
-export const changeMark = data => http.put('changeMark', data);
+export const payMent = (data) => http.post('pay', data.formData);
+export const changeMark = (data) => http.put('changeMark', data);
 export const getPreviewChat = () => http.get('getPreview');
-export const getDialog = data => http.get('getChat', {params: {
-    interlocutorId: data.interlocutorId
-  }});
-export const dataForContest = data => http.post('dataForContest', data);
-export const cashOut = data => http.post('cashout', data);
-export const updateUser = data => http.put('updateUser', data);
-export const newMessage = data => http.post('newMessage', data);
-export const changeChatFavorite = data => http.post('favorite', data);
-export const changeChatBlock = data => http.post('blackList', data);
-export const getCatalogList = data => http.get('getCatalogs', data);
-export const addChatToCatalog = data => http.post('addNewChatToCatalog', data);
-export const createCatalog = data => http.post('createCatalog', data);
-export const deleteCatalog = data => http.delete('deleteCatalog', data);
-export const removeChatFromCatalog = data =>
+export const getDialog = (data) =>
+  http.get('getChat', {
+    params: {
+      interlocutorId: data.interlocutorId,
+    },
+  });
+export const dataForContest = (data) => http.post('dataForContest', data);
+export const cashOut = (data) => http.post('cashout', data);
+export const updateUser = (data) => http.put('updateUser', data);
+export const newMessage = (data) => http.post('newMessage', data);
+export const changeChatFavorite = (data) => http.post('favorite', data);
+export const changeChatBlock = (data) => http.post('blackList', data);
+export const getCatalogList = (data) => http.get('getCatalogs', data);
+export const addChatToCatalog = (data) =>
+  http.post('addNewChatToCatalog', data);
+export const createCatalog = (data) => http.post('createCatalog', data);
+export const deleteCatalog = (data) => http.delete('deleteCatalog', data);
+export const removeChatFromCatalog = (data) =>
   http.delete('removeChatFromCatalog', data);
-export const changeCatalogName = data => http.put('updateNameCatalog', data);
-export const getCustomersContests = data =>
-  http.get(
-    'getCustomersContests',{ 
-      params:{
-        limit: data.limit,
-        offset: data.offset,
-        status: data.contestStatus
-      }
-    }
-  );
+export const changeCatalogName = (data) => http.put('updateNameCatalog', data);
+export const getCustomersContests = (data) =>
+  http.get('getCustomersContests', {
+    params: {
+      limit: data.limit,
+      offset: data.offset,
+      status: data.contestStatus,
+    },
+  });
 
 export const getActiveContests = ({
   offset,
@@ -46,23 +48,29 @@ export const getActiveContests = ({
   industry,
   awardSort,
   ownEntries,
-
 }) =>
   http.get('getAllContests', {
-    params:{
-    offset,
-    limit,
-    typeIndex,
-    contestId,
-    industry,
-    awardSort,
-    ownEntries,
-    }
+    params: {
+      offset,
+      limit,
+      typeIndex,
+      contestId,
+      industry,
+      awardSort,
+      ownEntries,
+    },
   });
 
-export const getContestById = data =>
+export const getContestById = (data) =>
   http.get('getContestById', {
     headers: {
       contestId: data.contestId,
     },
   });
+
+export const getOffers = (params) => {
+ return http.get('/offers', { params });
+};
+
+export const moderateOffer = (offerId, status) =>
+  http.patch(`/offers/${offerId}/moderate`, { status });
