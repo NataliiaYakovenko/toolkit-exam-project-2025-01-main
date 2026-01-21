7524e36a5a58a8d20808e5131b7dd5122a6ddc17
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-01-21 15:31:24 +0200

    Added offerRoter to index file

--- server/src/routers/index.js
+++ server/src/routers/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const chatRouter = require('./chatRouter');
 const contestRouter = require('./contestRouter');
 const userRouter = require('./userRouter');
+const offerRouter = require('./offerRouter');
 
 const router = express.Router();
 
 router.use('/', chatRouter);
 router.use('/', contestRouter);
 router.use('/', userRouter);
+router.use('/', offerRouter);
 
 module.exports = router;
