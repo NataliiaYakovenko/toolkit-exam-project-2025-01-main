8633dc2d20763f61bd5136ff480fa9c8ad7bbb0a
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-01-27 17:27:13 +0200

    Created migration conversations

---
+++ server/src/migrations/create-conversations.js
@@ -0,0 +1,25 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Conversations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Conversations');
+  },
+};
