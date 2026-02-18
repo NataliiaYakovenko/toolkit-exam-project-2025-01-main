8597731d0f2b700e207cddca5439b39573b8655f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-02-18 12:47:54 +0200

    Fixed adaptation and preserve focus styles on click

--- client/src/components/ButtonGroup/ButtonGroup.jsx
+++ client/src/components/ButtonGroup/ButtonGroup.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styles from './ButtonGroup.module.sass';
 import CONSTANTS from '../../constants';
 
 const ButtonGroup = () => {
+  const [selectedIndex, setSelectedIndex] = useState(null);
   const firstButton = useRef(null);
 
   useEffect(() => {
@@ -15,7 +16,12 @@ const ButtonGroup = () => {
         Do you want a matching domain (.com URL) with your name?
       </h3>
       <div className={styles.answersWrapper}>
-        <button ref={firstButton} className={styles.answerBox} type="button">
+        <button
+          ref={firstButton}
+          className={`${styles.answerBox} ${selectedIndex === 0 ? styles.selected : ''}`}
+          type="button"
+          onClick={() => setSelectedIndex(0)}
+        >
           <legend className={styles.answerMark}>Recommended</legend>
           <div className={styles.textWrapper}>
             <img src={CONSTANTS.BUTTON_GROUP_MARK} alt="Mark" />
@@ -23,14 +29,22 @@ const ButtonGroup = () => {
             <p>But minor variations are allowed</p>
           </div>
         </button>
-        <button className={styles.answerBox} type="button">
+        <button
+          className={`${styles.answerBox} ${selectedIndex === 1 ? styles.selected : ''}`}
+          type="button"
+          onClick={() => setSelectedIndex(1)}
+        >
           <div className={styles.textWrapper}>
             <img src={CONSTANTS.BUTTON_GROUP_MARK} alt="Mark" />
             <strong>Yes</strong>
             <p>The Domain should exactly match the name</p>
           </div>
         </button>
-        <button className={styles.answerBox} type="button">
+        <button
+          className={`${styles.answerBox} ${selectedIndex === 2 ? styles.selected : ''}`}
+          type="button"
+          onClick={() => setSelectedIndex(2)}
+        >
           <div className={styles.textWrapper}>
             <img src={CONSTANTS.BUTTON_GROUP_MARK} alt="Mark" />
             <strong>No</strong>
