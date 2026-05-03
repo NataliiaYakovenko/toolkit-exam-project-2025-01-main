72f27cc8654037233f7e62e26cc6520d3b14920c
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-05-03 14:59:53 +0300

    Converted the LoginForm component from class to functional

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Form, Formik } from 'formik';
 import { checkAuth, clearAuth } from '../../store/slices/authSlice';
@@ -8,79 +8,78 @@ import Schems from '../../utils/validators/validationSchems';
 import Error from '../Error/Error';
 import CONSTANTS from '../../constants';
 
-class LoginForm extends React.Component {
-  componentWillUnmount() {
-    this.props.authClear();
-  }
+const LoginForm = ({ auth, navigate, loginRequest, authClear, submitting }) => {
+  useEffect(() => {
+    return () => {
+      authClear();
+    };
+  }, [authClear]);
 
-  clicked = (values) => {
-    this.props.loginRequest({ data: values, navigate: this.props.navigate });
+  const clicked = (values) => {
+    loginRequest({ data: values, navigate });
   };
 
-  render() {
-    const { error, isFetching } = this.props.auth;
-    const { submitting, authClear } = this.props;
+  const { error, isFetching } = auth;
 
-    const formInputClasses = {
-      container: styles.inputContainer,
-      input: styles.input,
-      warning: styles.fieldWarning,
-      notValid: styles.notValid,
-      valid: styles.valid,
-    };
+  const formInputClasses = {
+    container: styles.inputContainer,
+    input: styles.input,
+    warning: styles.fieldWarning,
+    notValid: styles.notValid,
+    valid: styles.valid,
+  };
 
-    return (
-      <div className={styles.loginForm}>
-        {error && (
-          <Error
-            data={
-              error.data &&
-              typeof error.data === 'string' &&
-              error.data.trim().startsWith('<')
-                ? 'Server unavailable, try again later!'
-                : error.data?.message || error.data || 'Authorization error!'
-            }
-            status={error.status}
-            clearError={authClear}
+  return (
+    <div className={styles.loginForm}>
+      {error && (
+        <Error
+          data={
+            error.data &&
+            typeof error.data === 'string' &&
+            error.data.trim().startsWith('<')
+              ? 'Server unavailable, try again later!'
+              : error.data?.message || error.data || 'Authorization error!'
+          }
+          status={error.status}
+          clearError={authClear}
+        />
+      )}
+      <h2>LOGIN TO YOUR ACCOUNT</h2>
+      <Formik
+        initialValues={{
+          email: '',
+          password: '',
+        }}
+        onSubmit={clicked}
+        validationSchema={Schems.LoginSchem}
+      >
+        <Form>
+          <FormInput
+            classes={formInputClasses}
+            name="email"
+            type="text"
+            label="Email Address"
           />
-        )}
-        <h2>LOGIN TO YOUR ACCOUNT</h2>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          onSubmit={this.clicked}
-          validationSchema={Schems.LoginSchem}
-        >
-          <Form>
-            <FormInput
-              classes={formInputClasses}
-              name="email"
-              type="text"
-              label="Email Address"
-            />
-            <FormInput
-              classes={formInputClasses}
-              name="password"
-              type="password"
-              label="Password"
-            />
-            <button
-              type="submit"
-              disabled={submitting}
-              className={styles.submitContainer}
-            >
-              <span className={styles.inscription}>
-                {isFetching ? 'Submitting...' : 'LOGIN'}
-              </span>
-            </button>
-          </Form>
-        </Formik>
-      </div>
-    );
-  }
-}
+          <FormInput
+            classes={formInputClasses}
+            name="password"
+            type="password"
+            label="Password"
+          />
+          <button
+            type="submit"
+            disabled={submitting}
+            className={styles.submitContainer}
+          >
+            <span className={styles.inscription}>
+              {isFetching ? 'Submitting...' : 'LOGIN'}
+            </span>
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   const { auth } = state;
