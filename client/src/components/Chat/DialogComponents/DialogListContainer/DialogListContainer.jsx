62ee7ab228815eb259c441d2a49225a65fc6dc70
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-05-03 11:10:34 +0300

    Converted the DialogListContainer component from class to functional

--- client/src/components/Chat/DialogComponents/DialogListContainer/DialogListContainer.jsx
+++ client/src/components/Chat/DialogComponents/DialogListContainer/DialogListContainer.jsx
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { getPreviewChat } from '../../../../store/slices/chatSlice';
 import DialogList from '../DialogList/DialogList';
 
-class DialogListContainer extends React.Component {
-  componentDidMount() {
-    // this.props.getChatPreview();
-  }
+const DialogListContainer = ({ messagesPreview, userId, getPreviewChat }) => {
+  useEffect(() => {
+  }, [getPreviewChat]);
 
-  render() {
-    const { messagesPreview, userId } = this.props;
-    return <DialogList preview={messagesPreview} userId={userId} />;
-  }
-}
+  return <DialogList preview={messagesPreview} userId={userId} />;
+};
 
 const mapStateToProps = (state) => state.chatStore;
 
-const mapDispatchToProps = (dispatch) => ({
-  getChatPreview: () => dispatch(getPreviewChat()),
-});
+const mapDispatchToProps = {
+  getPreviewChat,
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(DialogListContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(DialogListContainer);
