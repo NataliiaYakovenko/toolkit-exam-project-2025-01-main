dcf80efb5e8c95d03e8f48d622752157c1bff75b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-05-12 16:44:28 +0300

    Improved  semantics in Questions component

--- client/src/components/HowItWorks/HowItWorksSection/Questions/Questions.jsx
+++ client/src/components/HowItWorks/HowItWorksSection/Questions/Questions.jsx
@@ -20,17 +20,17 @@ const Questions = () => {
   };
 
   return (
-    <div className={styles.section}>
+    <section className={styles.section}>
       <div className={styles.container}>
         <h3 className={styles.title}>Frequently Asked Questions</h3>
         <div className={styles.wrapperTypesQuestions}>
           {[
             'Launching A Contest',
             'Buying From Marketplace',
-            ' Managed Contests',
-            ' For Creatives',
+            'Managed Contests',
+            'For Creatives',
           ].map((item, index) => (
-            <span
+            <button
               key={index}
               onClick={() => handlerClick(index)}
               className={`${styles.typeQuestion} ${
@@ -38,13 +38,13 @@ const Questions = () => {
               }`}
             >
               {item}
-            </span>
+            </button>
           ))}
         </div>
 
         <TypesQuestions />
       </div>
-    </div>
+    </section>
   );
 };
 
