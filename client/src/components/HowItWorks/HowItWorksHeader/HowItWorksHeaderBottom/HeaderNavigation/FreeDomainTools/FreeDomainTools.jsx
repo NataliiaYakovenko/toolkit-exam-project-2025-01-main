c5924682fb6f4700aa38497a4728df8e7bbd8f1e
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-05-12 15:17:33 +0300

    Chanhed class component to functional and improved  semantics in FreeDomainTools

--- client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderBottom/HeaderNavigation/FreeDomainTools/FreeDomainTools.jsx
+++ client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderBottom/HeaderNavigation/FreeDomainTools/FreeDomainTools.jsx
@@ -3,45 +3,38 @@ import styles from './FreeDomainTools.module.sass';
 import FeatureCard from '../Common/FeatureCard/FeatureCard';
 import CONSTANTS from '../../../../../../constants';
 
-class FreeDomainTools extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const FreeDomainTools = () => {
+  return (
+    <>
+      <section className={styles.containerTools}>
+        <div className={styles.wrapperTools}>
+          <FeatureCard
+            link="https://www.atom.com/domain-name-generator"
+            icon={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_GENERATOR}
+            iconAlt="Generator"
+            title="AI Domain Name Generator"
+            description="Get hundreds of smart domain ideas in seconds."
+          />
 
-  render() {
-    return (
-      <>
-        <div className={styles.containerTools}>
-          <div className={styles.wrapperTools}>
-            <FeatureCard
-              link="https://www.atom.com/domain-name-generator"
-              icon={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_GENERATOR}
-              iconAlt="Generator"
-              title="AI Domain Name Generator"
-              description="Get hundreds of smart domain ideas in seconds."
-            />
+          <FeatureCard
+            link="https://www.atom.com/domain-appraisal"
+            icon={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_TOOL}
+            iconAlt="Tool"
+            title="AI Domain Appraisal Tool"
+            description="Instantly check your domain’s market"
+          />
 
-            <FeatureCard
-              link="https://www.atom.com/domain-appraisal"
-              icon={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_TOOL}
-              iconAlt="Tool"
-              title="AI Domain Appraisal Tool"
-              description="Instantly check your domain’s market"
-            />
-
-            <FeatureCard
-              link="https://www.atom.com/whois"
-              icon={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_LOOKUP}
-              iconAlt="LookUp"
-              title="WHOIS Domain Lookup"
-              description="Find domain ownership and registration details."
-            />
-          </div>
+          <FeatureCard
+            link="https://www.atom.com/whois"
+            icon={CONSTANTS.HOW_IT_WORKS_HEADER_ICON_LOOKUP}
+            iconAlt="LookUp"
+            title="WHOIS Domain Lookup"
+            description="Find domain ownership and registration details."
+          />
         </div>
-      </>
-    );
-  }
-}
+      </section>
+    </>
+  );
+};
 
 export default FreeDomainTools;
