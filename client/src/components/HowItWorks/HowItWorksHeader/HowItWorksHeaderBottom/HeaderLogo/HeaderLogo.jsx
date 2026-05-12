0f02a837f2d721e94945b4bd647e1e29e6f3c488
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-05-12 17:06:52 +0300

    Changed class HowItWorksHeaderLogo to functional

--- client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderBottom/HeaderLogo/HeaderLogo.jsx
+++ client/src/components/HowItWorks/HowItWorksHeader/HowItWorksHeaderBottom/HeaderLogo/HeaderLogo.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 import styles from './HeaderLogo.module.sass';
 import CONSTANTS from '../../../../../constants';
 
-class HowItWorksHeaderLogo extends React.Component {
-  render() {
-    return (
-      <>
-        <div className={styles.headerLogo}>
-          <a href="/">
-            <img src={CONSTANTS.HOW_IT_WORKS_HEADER_LOGO_ATOM} alt="logo" />
-          </a>
-        </div>
-      </>
-    );
-  }
-}
+const HowItWorksHeaderLogo = () => {
+  return (
+    <>
+      <div className={styles.headerLogo}>
+        <a href="/">
+          <img src={CONSTANTS.HOW_IT_WORKS_HEADER_LOGO_ATOM} alt="logo" />
+        </a>
+      </div>
+    </>
+  );
+};
 
 export default HowItWorksHeaderLogo;
