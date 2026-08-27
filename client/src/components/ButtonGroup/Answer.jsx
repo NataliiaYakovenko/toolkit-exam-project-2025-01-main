fe2337ebc9173a6c43b49c5c5e4ac6eb2da75531
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2026-08-27 13:49:42 +0300

    Extracted answers data and rendered buttons with map

---
+++ client/src/components/ButtonGroup/Answer.jsx
@@ -0,0 +1,20 @@
+const answers = [
+  {
+    id: 1,
+    mark: 'Recommended',
+    strong: 'Yes',
+    text: 'But minor variations are allowed',
+  },
+  {
+    id: 2,
+    strong: 'Yes',
+    text: 'The Domain should exactly match the name',
+  },
+  {
+    id: 3,
+    strong: 'No',
+    text: 'I am only looking for a name, not a Domain',
+  },
+];
+
+export default answers;
--- client/src/components/ButtonGroup/ButtonGroup.jsx
+++ client/src/components/ButtonGroup/ButtonGroup.jsx
@@ -1,39 +1,32 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import styles from './ButtonGroup.module.sass';
 import AnswerButton from './AnswerButton';
+import answers from './Answer';
 
 const ButtonGroup = () => {
-  const [selectedIndex, setSelectedIndex] = useState(null);
-
-  const handleSelect0 = useCallback(() => setSelectedIndex(0), []);
-  const handleSelect1 = useCallback(() => setSelectedIndex(1), []);
-  const handleSelect2 = useCallback(() => setSelectedIndex(2), []);
+  const [selectedId, setSelectedId] = useState(null);
 
   return (
     <div className={styles.buttonGroupContainer}>
       <h3 className={styles.buttonQuestion}>
         Do you want a matching domain (.com URL) with your name?
       </h3>
+
       <div className={styles.answersWrapper}>
-        <AnswerButton
-          selected={selectedIndex === 0}
-          onSelect={handleSelect0}
-          mark="Recommended"
-          strong="Yes"
-          text="But minor variations are allowed"
-        />
-        <AnswerButton
-          selected={selectedIndex === 1}
-          onSelect={handleSelect1}
-          strong="Yes"
-          text="The Domain should exactly match the name"
-        />
-        <AnswerButton
-          selected={selectedIndex === 2}
-          onSelect={handleSelect2}
-          strong="No"
-          text="I am only looking for a name, not a Domain"
-        />
+        {answers.map((answer) => {
+          return (
+            <AnswerButton
+              key={answer.id}
+              selected={selectedId === answer.id}
+              onSelect={() => {
+                setSelectedId(answer.id);
+              }}
+              mark={answer.mark}
+              strong={answer.strong}
+              text={answer.text}
+            />
+          );
+        })}
       </div>
     </div>
   );
